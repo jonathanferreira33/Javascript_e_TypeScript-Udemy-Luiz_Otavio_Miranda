5d372d940ebcbafba173e1c14e3bfb26bae70743
Jonathan Ferreira <jhouus33@gmail.com>
2021-07-11 20:01:25 -0300

    Ex calculadora IMC

---
+++ aulas/aula25/index.js
@@ -0,0 +1,9 @@
+const numero = 0;
+
+if (numero >= 0 && numero <= 5) {
+    console.log('Número está entre 0 e 5.');
+}else if (numero >= 6 && numero <= 10){
+    console.log('Número está entre 6 e 10.');
+}else{
+    console.log('Número não está entre 0 e 10.');
+}
